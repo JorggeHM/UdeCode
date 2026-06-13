cc62f59adce8e7cc1a0cf8e686d3877036a09fb1
Jorge Mota <94938902+JorggeHM@users.noreply.github.com>
2026-06-13 15:42:47 -0600

    CCFinished

--- CCFest/build/js/app.js
+++ CCFest/build/js/app.js
@@ -1,6 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
+    navegacionFija()
     crearGaleria()
+    resaltarEnlace()
+    scrollNav()
 })
+
+function navegacionFija() {
+    const header = document.querySelector('.header')
+    const sobreFestival = document.querySelector('.sobre-festival')
+
+    document.addEventListener('scroll', function () {
+        // console.log(sobreFestival.getBoundingClientRect().bottom)
+        if (sobreFestival.getBoundingClientRect().bottom < 1) {
+            header.classList.add('fixed')
+        } else {
+            header.classList.remove('fixed')
+
+        }
+    })
+
+}
 
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
@@ -12,24 +31,86 @@ function crearGaleria() {
 
         //Eventhandler
 
-        imagen.onclick = function(){
-            mostrarImagen()
+        imagen.onclick = function () {
+            mostrarImagen(i)
         }
 
         galeria.appendChild(imagen)
     }
 }
 
-function mostrarImagen(i){
+function mostrarImagen(i) {
+    const imagen = document.createElement('img')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = `Imagen Ejemplo ${i}`
     //Genera vista previa de img
     const modal = document.createElement('DIV')
     modal.classList.add('modal')
-    modal.onclick = cerrarModal    //Agrega al html
+    modal.onclick = cerrarModal
+
+    //boton cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+
+
+    //Agrega al html
     const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
     body.appendChild(modal)
 }
 
-function cerrarModal(){
-    const modal = document.querySelector('.modal')
-    modal?.remove()
+function cerrarModal() {
+    const modal = document.querySelector('.modal ')
+    modal.classList.add('fade-out')
+
+    setTimeout(() => {
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+
+    }, 500);
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function () {
+        const sections = document.querySelectorAll('section')
+        const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+        let actual = '';
+        sections.forEach(section => {
+
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+            if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
+                actual = section.id
+
+            }
+        })
+        navLinks.forEach(link => {
+            link.classList.remove('active')
+            if (link.getAttribute('href') === '#' + actual) {
+                link.classList.add('active')
+            }
+        })
+    })
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+
+            section.scrollIntoView({ behavior: 'smooth' })
+        });
+    })
 }
